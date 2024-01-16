b51dfdc8944d03d3e5316936bcb55d486596967b
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-01-16 01:39:47 +0100

    images configuration

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () =>{
     return(
-        <div>About Page</div>
+        <div>
+            <div className={styles.imgConteiner}> 
+                {/* <Image src="/about.png"  alt="" fill /> */}
+
+                <Image src="https://images.pexels.com/photos/17322370/pexels-photo-17322370/free-photo-of-maos-internet-refeicao-comida.jpeg"  alt="" fill />
+            </div>
+        </div>
     )
 }
 
